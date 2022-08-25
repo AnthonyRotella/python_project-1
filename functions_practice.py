093ab9c1c44ed3214071b6e27f53cd29f3a2a93e
Anthony Rotella <mr4406@yahoo.com>
2022-08-24 19:48:38 -0500

    Add files via upload

---
+++ functions_practice.py
@@ -0,0 +1,20 @@
+# def hello():
+#   print("Hello, user!")
+#   hello()
+
+
+#   def pack(a,b,c):
+#   return [a,b,c]
+#   print(pack("a","b","c"))
+
+
+#   def eat_lunch(my_lst):
+#   if len(my_lst) == 0:
+#     print("My lunchbox is empty!")
+#   else:
+#     for i in range(len(my_lst)):
+#       if i == 0:
+#         print(f"First I eat {my_lst[0]}")
+#       else:
+#         print(f"Next I eat {my_lst[i]}")
+# eat_lunch(["apple","banana","sandwich","cookie"])
